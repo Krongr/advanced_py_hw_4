b7bdaf7a317533bab2add9e143407c9cddc077f3
Artem Nazarov <krongr.study@ya.ru>
2022-03-17 18:12:14 +0300

    Initial commit

---
+++ generator.py
@@ -0,0 +1,18 @@
+def unp_generator(list_to_unp):
+    if type(list_to_unp) != list:
+        yield list_to_unp
+    else:  
+        for _i in list_to_unp:
+            for _k in unp_generator(_i):
+                yield _k
+
+if __name__ == '__main__':
+    nested_list = [
+  	    ['a', 'b', 'c'],
+  	    ['d', 'e', ['f', ['h']], False],
+  	    [1, 2, None],
+     ]
+  
+  
+    for item in unp_generator(nested_list):
+        print(item)
